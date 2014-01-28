d22fdeafe1d5bfeb3f878ad18e1be2c3ac644edb
andre <andre@andre-VirtualBox.(none)>
2014-01-28 09:50:46 +0100

    integration...

--- trainingapp-service/src/main/java/service/TodoService.java
+++ trainingapp-service/src/main/java/service/TodoService.java
@@ -1,10 +1,22 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Required;
+
+import dao.TodoDao;
 import model.Todo;
 
 public class TodoService {
 
+	private TodoDao todoDao;
+	
 	public void storeTodo(Todo todo){
 		
 	}
+
+	@Required
+	public void setTodoDao(TodoDao todoDao) {
+		this.todoDao = todoDao;
+	}
+	
+	
 }
